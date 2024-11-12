cd5fab247e3b1a179658f4fce743645f91da6c5c
Strocs <strocsdev@gmail.com>
2024-11-12 15:54:36 -0300

    update: have more points

--- halloween.dev/2-zombie-horde/index.js
+++ halloween.dev/2-zombie-horde/index.js
@@ -1,10 +1,12 @@
 function battleHorde(zombies, humans) {
-  let singleBattleResult = 0
+  let war = 0
   for (let index = 0; index < zombies.length; index++) {
-    singleBattleResult += zombies[index] - humans[index]
+    war += zombies[index] - humans[index]
   }
-  if (singleBattleResult === 0) return 'x'
-  return `${Math.abs(singleBattleResult)}${singleBattleResult > 0 ? 'z' : 'h'}`
+  if (war === 0) return 'x'
+  const letter = war > 0 ? 'z' : 'h'
+  const battleResult = Math.abs(war)
+  return `${battleResult}${letter}`
 }
 
 export default battleHorde
--- halloween.dev/4-find-the-killer/index.js
+++ halloween.dev/4-find-the-killer/index.js
@@ -1,20 +1,35 @@
 function findTheKiller(whisper, suspects) {
-  let killers = []
-  const whisperLength = whisper.length - +whisper.includes('$')
-  for (const suspect of suspects) {
-    let isSuspect = whisperLength <= suspect.length
-    let suspectName = suspect.toLowerCase()
-    for (let letter = 0; letter < whisper.length && isSuspect; letter++) {
-      if (whisper[letter] === '~') continue
-      if (whisper[letter] === '$') {
-        isSuspect = whisperLength === suspect.length
-        break
-      }
-      isSuspect = whisper[letter] === suspectName[letter]
-    }
-    isSuspect && killers.push(suspect)
+  let endOfWhisper = ''
+  if (whisper.endsWith('$')) {
+    whisper.slice(0, -1)
+    endOfWhisper = '$'
+  } else {
+    endOfWhisper = '.*'
   }
-  return killers.toString()
+  const whisperWild = whisper.replaceAll('~', '.')
+  const clearedWhisper = `^${whisperWild}${endOfWhisper}`
+  const whisperRegex = new RegExp(clearedWhisper, 'i')
+  const killers = suspects.filter(suspect => whisperRegex.test(suspect))
+  return killers.join()
 }
+
+// function findTheKiller(whisper, suspects) {
+//   let killers = []
+//   const whisperLength = whisper.length - +whisper.includes('$')
+//   for (const suspect of suspects) {
+//     let isSuspect = whisperLength <= suspect.length
+//     let suspectName = suspect.toLowerCase()
+//     for (let letter = 0; letter < whisper.length && isSuspect; letter++) {
+//       if (whisper[letter] === '~') continue
+//       if (whisper[letter] === '$') {
+//         isSuspect = whisperLength === suspect.length
+//         break
+//       }
+//       isSuspect = whisper[letter] === suspectName[letter]
+//     }
+//     isSuspect && killers.push(suspect)
+//   }
+//   return killers.toString()
+// }
 
 export default findTheKiller
