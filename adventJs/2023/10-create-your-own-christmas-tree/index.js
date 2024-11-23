39d058a346b24c4d4541b5f7e7b5c5f8e3f90982
Strocs <strocsdev@gmail.com>
2024-11-22 21:54:03 -0300

    add problem 10 not resolved

---
+++ adventJs/2023/10-create-your-own-christmas-tree/index.js
@@ -0,0 +1,12 @@
+function createChristmasTree(ornaments, height) {
+  const lineJump = '\n'
+  const trunk = ' '.repeat(height - 1) + lineJump
+
+  for (let i = -0; i < height; i++) {
+
+  }
+
+  return '|'
+}
+
+export { createChristmasTree }
---
+++ adventJs/2023/10-create-your-own-christmas-tree/index.test.js
@@ -0,0 +1,33 @@
+import { describe, expect, test } from 'bun:test'
+import { createChristmasTree } from './index.js'
+
+describe('create-your-own-christmas-tree', () => {
+  test('1', () => {
+    const result = typeof createChristmasTree('x', 3)
+    expect(result).toBe('string')
+  })
+
+  test('2', () => {
+    const result = createChristmasTree(x, 3)
+    const expected = "  x\n x x\nx x x\n  |\n"
+    expect(result).toBe(expected)
+  })
+
+  test('3', () => {
+    const result = createChristmasTree("xo", 4)
+    const expected = "   x\n  o x\n o x o\nx o x o\n   |\n"
+    expect(result).toBe(expected)
+  })
+
+  test('4', () => {
+    const result = createChristmasTree(123, 5)
+    const expected = "   1\n   2 3\n  1 2 3\n 1 2 3 1\n2 3 1 2 3\n    |\n"
+    expect(result).toBe(expected)
+  })
+
+  test('5', () => {
+    const result = createChristmasTree("*@o", 3)
+    const expected = "  *\n @ o\n* @ o\n  |\n"
+    expect(result).toBe(expected)
+  })
+})
