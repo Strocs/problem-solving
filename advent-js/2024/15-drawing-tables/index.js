bdbbec12cf7d90dbcad55124b9172a6662583202
Strocs <strocsdev@gmail.com>
2024-12-17 13:41:26 -0300

    solution for callenge 15 on advent js 2024

--- advent-js/2024/15-drawing-tables/index.js
+++ advent-js/2024/15-drawing-tables/index.js
@@ -1,79 +1,41 @@
 /**
-  * @param {Array<Object>} data
-  * @returns {string}
-  */
+ * @param {Array<Object>} data
+ * @returns {string}
+ */
+
 function drawTable(data) {
-  // need width of cols
-  // need number of cols
-
-  let colsWidth = []
-  let cols = []
-  let prevSpace = null
-
-  for (let rows of data) {
-    const row = Object.entries(rows)
-
-    for (let i = 0; i < row.length; i++) {
-      const [head, body] = row[i]
-      // create the array if not exist
-      cols[i] = cols[i] ?? []
-      cols[i][0] =
-        `+${'-'.repeat(head.length + 2)}
-| ${head} 
-+${'-'.repeat(head.length + 2)}`
-      cols[i].push(`| ${body} `)
-
-      const longest = head.length > body.length ? head.length : body.length
-      colsWidth[i] = +colsWidth[i] > longest ? colsWidth[i] ?? 0 : longest
-
-      if (prevSpace) {
-
-      }
+  let w = [],
+    c = [[]];
+  for (let r = 0; r < data.length; r++) {
+    const d = Object.entries(data[r]);
+    let t = [];
+    for (let i = 0; i < d.length; i++) {
+      const [h, b] = d[i];
+      if (r === 0) c[0].push(h[0].toUpperCase() + h.slice(1));
+      t.push(b.toString());
+      const l = h.length > b.toString().length ? h.length : b.toString().length;
+      w[i] = +w[i] > l ? (w[i] ?? 0) : l;
     }
-
-    console.log(cols.join(''))
-
-    // for (let j = 0; j < entries.length; j++) {
-    //   const [head, body] = entries[j]
-    //   if (!colsHead[j]) colsHead[j] = head
-    //   colsBody[j] = !!colsBody[j] ? [...colsBody[j], body] : [body]
-    //   const longest = head.length > String(body).length ? head.length : String(body).length
-    //   colsWidth[j] = +colsWidth[j] > longest ? colsWidth[j] ?? 0 : longest
-    // }
+    c.push(t);
   }
-  return ''
+  let t = "";
+  let hs = "";
+  for (let i = 0; i < c.length; i++) {
+    let s = "";
+    let h = "";
+    for (let j = 0; j < c[i].length; j++) {
+      h += "+" + "-".repeat(w[j] + 2);
+      s += "| " + c[i][j] + " ".repeat(w[j] - c[i][j].length + 1);
+    }
+    h += "+";
+    s += "|\n";
+    if (!hs) {
+      hs = h;
+      t += hs + "\n" + s + hs + "\n";
+      continue;
+    }
+    t += s;
+  }
+  t += hs;
+  return t;
 }
-
-const f = drawTable([
-  { name: 'Alice', city: 'London' },
-  { name: 'Bob', city: 'Paris' },
-  { name: 'Charlie', city: 'New York' }
-])
-// +---------+-----------+
-// | Name    | City      |
-// +---------+-----------+
-// | Alice   | London    |
-// | Bob     | Paris     |
-// | Charlie | New York  |
-// +---------+-----------+
-
-// | Name 
-// | Alice 
-// | Bob 
-// | Charlie 
-
-
-
-const x = drawTable([
-  { gift: 'Doll', quantity: 10 },
-  { gift: 'Book', quantity: 5 },
-  { gift: 'Music CD', quantity: 1 }
-])
-// +----------+----------+
-// | Gift     | Quantity |
-// +----------+----------+
-// | Doll     | 10       |
-// | Book     | 5        |
-// | Music CD | 1        |
-
-console.log(f + '\n' + x)
