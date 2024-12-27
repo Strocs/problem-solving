fd9e5240a56ac8c5c95530b9202a422e02a3901e
Strocs <strocsdev@gmail.com>
2024-12-26 23:07:39 -0300

    solution of challenge 18 of adventjs 2024

--- advent-js/2024/18-santas-magic-agenda/index.js
+++ advent-js/2024/18-santas-magic-agenda/index.js
@@ -11,30 +11,18 @@ function findInAgenda(agenda, phone) {
     if (!agen.includes(phone)) continue;
     if (result) return null;
 
-    const nameRegex = /<([A-Za-z\s]+)>/g;
-    const addressRegex =
-      /([A-Za-z0-9\s,]+(?:\s[A-Za-z0-9\s,]+)*)(?=\s*(?!\+?\d{1,2}-\d{3}-\d{3}-\d{3})\s*$)/g;
-    const name = nameRegex.exec(agen)[1].trim();
-    const address = addressRegex.exec(agen);
+    const name = agen.match(/(?<=<)[A-Za-z\s]+(?=>)/g)[0];
+    const fullPhone = agen.match(
+      /\+([0-9]{1,2}-[0-9]{3}-[0-9]{3}-[0-9]{3,4})/g,
+    )[0];
+    let address = agen
+      .replace(name, "")
+      .replace("<", "")
+      .replace(">", "")
+      .replace(fullPhone, "")
+      .trim();
+
     result = { name, address };
   }
   return result;
 }
-
-const agenda = `+34-600-123-456 Calle Gran Via 12 <Juan Perez>
-Plaza Mayor 45 Madrid 28013 <Maria Gomez> +34-600-987-654
-<Carlos Ruiz> +1-800-555-0199 Fifth Ave New York`;
-
-console.log(findInAgenda(agenda, "34-600-123-456"));
-// { name: "Juan Perez", address: "Calle Gran Via 12" }
-
-console.log(findInAgenda(agenda, "600-987"));
-// { name: "Maria Gomez", address: "Plaza Mayor 45 Madrid 28013" }
-
-console.log(findInAgenda(agenda, "111"));
-// null
-// Explanation: No results
-
-console.log(findInAgenda(agenda, "1"));
-// null
-// Explanation: Too many results
