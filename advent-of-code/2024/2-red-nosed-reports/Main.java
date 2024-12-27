c80ad98bc1ac58643b1192329437976c57363c3c
Strocs <strocsdev@gmail.com>
2024-12-26 22:35:40 -0300

    solution of challenge 2 of advent of code 2024

--- advent-of-code/2024/2-red-nosed-reports/Main.java
+++ advent-of-code/2024/2-red-nosed-reports/Main.java
@@ -1,84 +1,86 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    List<int[]> input = getInput();
-    PartOne(input);
-    PartTwo(input);
-  }
+    List<List<Integer>> input = getInput();
 
-  private static void PartOne(List<int[]> input) {
-    int partOne = 0;
-    for (int[] line : input) {
-      Boolean last = null;
-      for (int i = 1; i < line.length; i++) {
-        int diff = line[i] - line[i - 1];
-        if (Math.abs(diff) < 1 || Math.abs(diff) > 3 || last != null && last != diff < 0) {
-          continue;
-        }
+    int firstResult = 0;
+    int secondResult = 0;
 
-        last = diff < 0;
-      }
-      partOne++;
-    }
-    System.out.println("Part One: " + partOne);
-  }
+    for (List<Integer> line : input) {
+      if (firstPuzzle(line))
+        firstResult++;
 
-  private static void PartTwo(List<int[]> input) {
-    int partTwo = 0;
-
-    for (int[] line : input) {
-
-      Boolean last = null;
-      Integer idxToAvoid = null;
-
-      for (int i = 1; i < line.length; i++) {
-        int diff = line[i] - line[i - 1];
-
-        // Fix this
-        if (Math.abs(diff) < 1 || Math.abs(diff) > 3 || last != null && last != diff < 0) {
-          if (idxToAvoid != null)
-            break;
-          idxToAvoid = i;
-          continue;
-        }
-
-        last = diff < 0;
-      }
-      partTwo++;
+      if (secondPuzzle(line))
+        secondResult++;
     }
 
-    System.out.println("Part Two: " + partTwo);
+    System.out.println("First puzzle: " + firstResult);
+    System.out.println("Second puzzle: " + secondResult);
   }
 
-  private static List<int[]> getInput() {
+  private static boolean firstPuzzle(List<Integer> line) {
+    if (isSafe(line)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  private static boolean secondPuzzle(List<Integer> line) {
+    for (int i = 0; i < line.size(); i++) {
+      List<Integer> newList = new ArrayList<>(line);
+      newList.remove(i);
+
+      if (isSafe(newList)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private static boolean isSafe(List<Integer> line) {
+    boolean isSafe = true;
+    for (int j = 1; j < line.size(); j++) {
+      int diff = line.get(j) - line.get(j - 1);
+      if (!isRangeValid(diff) || j > 1 && !isDirectionValid(line.get(j - 1) - line.get(j - 2), diff)) {
+        isSafe = false;
+        break;
+      }
+    }
+    return isSafe;
+  }
+
+  private static boolean isRangeValid(int diff) {
+    return Math.abs(diff) >= 1 && Math.abs(diff) <= 3;
+  }
+
+  private static boolean isDirectionValid(int prevDiff, int currDiff) {
+    if (currDiff == 0)
+      return false;
+    return (prevDiff > 0 && currDiff > 0) || (prevDiff < 0 && currDiff < 0);
+  }
+
+  private static List<List<Integer>> getInput() {
     try {
-      Scanner scanner = new Scanner(new File("input.txt"));
-      List<int[]> result = new ArrayList<int[]>();
+      return Files.readAllLines(Paths.get("input.txt"))
+          .stream()
+          .map(line -> Arrays.stream(line.split(" "))
+              .map(Integer::parseInt)
+              .toList())
+          .toList();
 
-      while (scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        String[] sep = line.split(" ");
-        int[] parsedLine;
-        parsedLine = new int[sep.length];
-
-        for (int i = 0; i < sep.length; i++) {
-          parsedLine[i] = Integer.parseInt(sep[i]);
-        }
-        result.add(parsedLine);
-      }
-
-      scanner.close();
-      return result;
-
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException("File not found");
+    } catch (IOException e) {
+      throw new RuntimeException("File not found", e);
     }
-
   }
 }
